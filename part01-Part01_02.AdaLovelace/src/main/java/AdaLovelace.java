8dce044f792d2a056174f9f9d95395be309c60b6
CHAMA-BOU <bouizgarchaimaa@gmail.com>
2023-09-26 02:33:31 +0100

    Add exo_2-Part_1

---
+++ part01-Part01_02.AdaLovelace/src/main/java/AdaLovelace.java
@@ -0,0 +1,10 @@
+
+public class AdaLovelace {
+
+    public static void main(String[] args) {
+        // Write your program here
+        System.out.println("Ada Lovelace");
+        int integer = 3.0 / 2;
+        System.out.println(integer);
+    }
+}
