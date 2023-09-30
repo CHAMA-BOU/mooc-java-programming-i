acacd70047b7f5290422160f93e6592f414767a4
CHAMA-BOU <bouizgarchaimaa@gmail.com>
2023-09-30 21:53:58 +0100

    Part 6 exos

---
+++ part06-Part06_05.SantasWorkshop/src/main/java/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        Package pachGifts= new Package();
+        System.out.println(pachGifts.totalWeight());
+
+        // use this main method to try out your classes!
+
+    }
+}
